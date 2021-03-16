2d70bfc55173f1e31529a48c6eddceb7b16c2413
KySo4oK <prostorostuk08@gmail.com>
2021-03-16 20:22:24 +0200

    add first test - just from example

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,32 @@
 package org.example;
 
-import static org.junit.Assert.assertTrue;
-
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+    public WebDriver driver;
+
+    @Before
+    public void SetUp() {
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
+        System.out.println("Test start");
+    }
+
     @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void shouldNotRegisterUserWhenProvidedPasswordLessFourSymbols() {
+        driver.get("http://prom.ua/join-customer?source_id=txt.register.customer");
+        String title = driver.getTitle();
+        Assert.assertTrue(title.equals("Регистрация покупателя на сайте Prom.ua"));
+    }
+
+    @After
+    public void close() {
+        System.out.println("Test end");
+        driver.quit();
     }
 }
