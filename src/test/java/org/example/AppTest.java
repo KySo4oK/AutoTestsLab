584394c461b8f6d0e6323776eeb582712e39eebc
KySo4oK <prostorostuk08@gmail.com>
2021-03-16 20:38:35 +0200

    add test with full xpath

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -4,8 +4,10 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class AppTest {
     public WebDriver driver;
@@ -20,6 +22,12 @@ public class AppTest {
     @Test
     public void shouldNotRegisterUserWhenProvidedPasswordLessFourSymbols() {
         driver.get("http://prom.ua/join-customer?source_id=txt.register.customer");
+        driver.findElement(By.xpath("/html/body/div[5]/div[2]/div/div[2]/div/form/div[2]/input")).sendKeys("rostyslav");
+        driver.findElement(By.xpath("/html/body/div[5]/div[2]/div/div[2]/div/form/div[3]/input")).sendKeys("123email@google.com");
+        driver.findElement(By.xpath("/html/body/div[5]/div[2]/div/div[2]/div/form/div[4]/input")).sendKeys("123");
+        driver.findElement(By.xpath("/html/body/div[5]/div[2]/div/div[2]/div/form/div[7]/button")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
         String title = driver.getTitle();
         Assert.assertTrue(title.equals("Регистрация покупателя на сайте Prom.ua"));
     }
