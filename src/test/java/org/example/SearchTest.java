b4816a19db8d2c9b6ef052c556b12d0f9b80bdfc
KySo4oK <prostorostuk08@gmail.com>
2021-03-30 00:06:47 +0300

    add options to driver

--- src/test/java/org/example/SearchTest.java
+++ src/test/java/org/example/SearchTest.java
@@ -20,9 +20,9 @@ public class SearchTest {
     public void SetUp() {
         System.setProperty("webdriver.chrome.driver", "src/main/resources/drivers/chromedriver");
         ChromeOptions options = new ChromeOptions();
-        options.addArguments("--single-process");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--single-process");
         driver = new ChromeDriver(options);
     }
 
